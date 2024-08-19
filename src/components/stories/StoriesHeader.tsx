00417a98f857cfcc405e04aea1db9f6fea4ab567
DizzlyD <david4henery00@hotmail.com>
2024-08-19 13:41:36 +0100

    stories-page-responsive

--- src/components/stories/StoriesHeader.tsx
+++ src/components/stories/StoriesHeader.tsx
@@ -1,20 +1,25 @@
 import { Heading, FourthHeading, BaseText } from "../reusable/text";
 import Image from "next/image";
-import { moonOfAppalaciaImgMob } from "../../../public/assets/stories/mobile";
+import { moonOfAppalaciaImgMob, moonOfAppalaciaImgMobLg } from "../../../public/assets/stories/mobile";
+import { moonOfAppalaciaImgTab, moonOfAppalaciaImgTabLg } from "../../../public/assets/stories/tablet";
+import { moonOfAppalaciaImgDesk, moonOfAppalaciaImgDeskLg } from "../../../public/assets/stories/desktop";
 import Button from "../reusable/buttons/Button";
 import ArrowIcon from "../icons/ArrowIcon";
 
 const StoriesHeader = () => {
   return (
-    <header className="w-full col-start-1 col-end-13 bg-black">
-      <Image
-        src={moonOfAppalaciaImgMob}
-        alt="A misty forest with mountains in Appalachia at night with clear skies and a full moon"
-        priority={true}
-        quality={100}
-        className="object-cover object-center"
-      />
-      <div className="w-full px-6 py-12">
+    <header className="w-full col-start-1 col-end-13 bg-black smTab:grid smTab:grid-cols-overallMob md2Tab:grid-cols-overallTab desk:grid-cols-overallDesk">
+      <div className="w-full relative smTab:col-start-1 smTab:col-end-13 smTab:row-start-1 smTab:row-end-2">
+        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-white/0 to-black/65"></div>
+        <Image
+          src={moonOfAppalaciaImgDeskLg}
+          alt="A misty forest with mountains in Appalachia at night with clear skies and a full moon"
+          priority={true}
+          quality={100}
+          className="w-full object-cover object-center smTab:h-full smTab:max-h-[650px]"
+        />
+      </div>
+      <div className="w-full px-6 py-12 z-10 max-w-[387px] smTab:col-start-2 smTab:col-end-12 smTab:row-start-1 smTab:row-end-2 smTab:px-0 smTab:py-[122px] smTab:self-center">
         <div className="grid gap-4">
           <FourthHeading theme="dark">
             LAST MONTH’S FEATURED STORY
@@ -28,13 +33,17 @@ const StoriesHeader = () => {
         <div className="w-full mt-6">
           <BaseText theme="dark">
             The dissected plateau area, while not actually made up of geological
-            mountains, is popularly called &quot;mountains,&quot; especially in eastern
-            Kentucky and West Virginia, and while the ridges are not high, the
-            terrain is extremely rugged.
+            mountains, is popularly called &quot;mountains,&quot; especially in
+            eastern Kentucky and West Virginia, and while the ridges are not
+            high, the terrain is extremely rugged.
           </BaseText>
-          <Button buttonType="primary" theme="primaryDark" className="uppercase mt-6">
+          <Button
+            buttonType="primary"
+            theme="primaryDark"
+            className="uppercase mt-6"
+          >
             Read the story
-            <ArrowIcon stroke="#fff"/>
+            <ArrowIcon stroke="#fff" />
           </Button>
         </div>
       </div>
